11fba8e14c4d02d4a5d7d46afc3fe957eceacee6
jindacz <jindaz.work@outlook.com>
2023-04-04 20:28:16 -0700

    update java doc of Tea.java

--- src/tea/Tea.java
+++ src/tea/Tea.java
@@ -1,18 +1,39 @@
 package tea;
 
+/**
+ * Abstract class for Tea.
+ */
 public abstract class Tea {
   Size size = Size.LARGE;
   String description = "Unknown Tea";
+
+  /**
+   * Get menu description of the tea.
+   * @return a celsius temperature.
+   */
   public String getDescription() {
     return this.description;
   }
+
+  /**
+   * Set the size of tea.
+   * @param size size of the tea.
+   */
   public void setSize(Size size) {
     this.size = size;
   }
 
+  /**
+   * Get the size of tea.
+   * @return Size of tea.
+   */
   public Size getSize() {
     return this.size;
   }
 
+  /**
+   * Calculate the price of tea.
+   * @return price of tea.
+   */
   public abstract double price();
 }
