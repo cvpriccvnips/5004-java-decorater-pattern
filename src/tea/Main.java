bbc3d4d775ede1fba230f725c86ee60de02d2b88
jindacz <jindaz.work@outlook.com>
2023-04-07 23:39:22 -0700

    add main method

--- src/tea/Main.java
+++ src/tea/Main.java
@@ -2,6 +2,10 @@ package tea;
 
 public class Main {
   public static void main(String[] args) {
+    Tea greenTea = new GreenTea(Size.LARGE);
+    greenTea = new TapiocaTopping(greenTea);
 
+    System.out.println("You ordered: " + greenTea.getDescription());
+    System.out.println("Total price: $" + greenTea.price());
   }
 }
