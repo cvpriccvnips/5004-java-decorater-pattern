890c23da1f3d1a969760dc50b9f70379ce5beadc
judywangyq <judywangyq@gmail.com>
2023-04-09 14:11:14 -0700

    add pudding topping

--- src/tea/PuddingTopping.java
+++ src/tea/PuddingTopping.java
@@ -1,2 +1,37 @@
-package tea;public class PuddingTopping {
+package tea;
+
+/**
+ * Class for adding Tapio topping to tea.
+ */
+public class PuddingTopping extends ToppingDecorator {
+  /**
+   * Constructs PuddingTopping for the tea.
+   *
+   * @param tea tea that we want to add pudding topping.
+   */
+  public PuddingTopping(Tea tea) {
+    this.tea = tea;
+  }
+
+  public String getDescription() {
+    return tea.getDescription() + ", you added Pudding";
+  }
+
+  /**
+   * Calculate the price of tea.
+   *
+   * @return price of tea.
+   */
+  @Override
+  public double price() {
+    double price = tea.price();
+    if (tea.getSize() == Size.REGULAR) {
+      price += 0.8;
+    } else if (tea.getSize() == Size.LARGE) {
+      price += 1.3;
+    } else if (tea.getSize() == Size.EXTRA_LARGE) {
+      price += 1.8;
+    }
+    return price;
+  }
 }
